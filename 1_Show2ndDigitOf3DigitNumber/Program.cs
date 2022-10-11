13c128754db3c3afcc7e0fefb32057a8b6bc7312
Renat Prokopiev <prokop4891@gmail.com>
2022-10-11 20:35:52 +0300

    1st

---
+++ 1_Show2ndDigitOf3DigitNumber/Program.cs
@@ -0,0 +1,5 @@
+﻿int N = new Random().Next(100,1000);
+Console.WriteLine(N);
+int N1 = N / 10;
+int n = N1 % 10;
+Console.WriteLine(n);
---
+++ 2_3rdDigitAvailability/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Enter number ");
+string number = Console.ReadLine();
+Console.Write("The number entered has ");
+Console.Write(number.Length);
+Console.WriteLine(" digit(-s).");
+if (number.Length < 3)
+{
+    Console.WriteLine("There are no 3rd digit.");
+}
+else
+{
+   int N = int.Parse(number);
+   int A = number.Length - 3;
+   if (A==0)
+   {
+        int n = N % 10;
+        Console.WriteLine(n);
+   }
+   else
+   {
+        int N1 = N/(int)Math.Pow(10,A);
+        int N2 = N1 % 10;
+        Console.WriteLine(N2);
+   }
+}
